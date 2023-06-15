71f0756ef335f69258798153a5ea2fd91a872392
enzosgporto <enzosgporto@hotmail.com>
2023-06-15 11:14:36 -0500

    Added rough outline for adding universities

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
+import locationClasses.country;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Scanner;
 
 import universityClasses.university;
 
@@ -19,6 +21,32 @@ public class Main {
         JsonArray main = new JsonArray();
         Type uniList = new TypeToken<List<university>>() {}.getType();
         List<university> list = gson.fromJson(loadFileFromClasspath("universityList.json"), uniList);
+
+        //loop for adding universities
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you wish to add a university or an american university?");
+        String universityToAdd = sc.nextLine();
+        if(universityToAdd.equalsIgnoreCase("university")){
+            System.out.println("What is the name of this university?");
+            String uniName = sc.nextLine();
+
+            System.out.println("What is the city this university is located");
+            String uniCity = sc.nextLine();
+
+            System.out.println("In which state is this university located");
+            String uniState = sc.nextLine();
+
+            System.out.println("Does this university accept financial aid: (y/n)");
+            String fa = sc.nextLine();
+            Boolean answer = false;
+            if(fa.equalsIgnoreCase("y")){
+                answer = true;
+            }
+            university university = new university(uniName, new country("Yes", "Yes"), uniCity, uniState, answer);
+
+        } else if (universityToAdd.equalsIgnoreCase("american university")) {
+
+        }
 
     }
 
